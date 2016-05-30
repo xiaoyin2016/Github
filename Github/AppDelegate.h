4bf60e8e4b6041cd9929b81375c878c99e21b718
xiaoyin <xiaoyin@macs-macbook-pro-2.local>
2016-05-30 09:25:34 +0800

    Initial Commit

---
+++ Github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github
+//
+//  Created by xiaoyin on 16/5/30.
+//  Copyright © 2016年 xiaoyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
